4b2246dfdd6e65c080a70ad09a2072e265a9d1e2
Renan Toniolo Rocha <renantoniolo@gmail.com>
2020-01-12 22:19:39 -0300

    inclusao testes unitários
    incluido testes unitários

---
+++ TesteUnitario/LocalWeatherViewModelTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using ClimaTempo.Http;
+using NUnit.Framework;
+
+namespace TesteUnitario
+{
+    [TestFixture()]
+    public class LocalWeatherViewModelTest
+    {
+
+        [Test()]
+        public async Task Test_Return_City_isNotNull()
+        {
+
+            // Arrange
+            var local = await ServiceHttp.GetCity(-23.620635, -46.607222).ConfigureAwait(false);
+
+            // Assert
+            Assert.IsNotNull(local);
+
+        }
+
+        [Test()]
+        public async Task Test_Return_City_equals_sao_paulo()
+        {
+
+            // Arrange
+            var local = await ServiceHttp.GetCity(-23.620635, -46.607222).ConfigureAwait(false);
+
+            //Act
+            var very_local = "São Paulo";
+
+            // Assert
+            Assert.AreSame(local, very_local);
+
+        }
+    }
+}
+
+
